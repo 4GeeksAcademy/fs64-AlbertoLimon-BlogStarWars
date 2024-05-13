49ac7d99ba99a299ab8714effdffd0cc2c19072b
Alberto Limón <119005684+albertolimongithub@users.noreply.github.com>
2024-05-13 04:01:17 +0000

    first commit

--- src/js/layout.js
+++ src/js/layout.js
@@ -22,7 +22,7 @@ const Layout = () => {
 				<ScrollToTop>
 					<Navbar />
 					<Routes>
-						<Route path="/" element={<Home />} />
+						<Route exact path="/" element={<Home />} />
 						<Route path="/demo" element={<Demo />} />
 						<Route path="/single/:theid" element={<Single />} />
 						<Route path="*" element={<h1>Not found!</h1>} />
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,55 @@
-import React from "react";
+import React, { useContext } from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import { Context } from "../store/appContext";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+export const Home = () => {
+
+	const { store, actions} = useContext(Context);
+
+	return (
+		<div className="container-fluid">
+		<nav className="navbar navbar-light bg-light">
+  			<div className="container-fluid">
+    			<a className="navbar-brand">Navbar</a>
+    			<div className="dropdown">
+  					<button className="btn btn-secondary dropdown-toggle bg-primary" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+    					Dropdown button
+  					</button>
+  				<ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+    				<li><a className="dropdown-item" href="#">Action</a></li>
+    				<li><a className="dropdown-item" href="#">Another action</a></li>
+    				<li><a className="dropdown-item" href="#">Something else here</a></li>
+  				</ul>
+				</div>
+  			</div>
+		</nav>
+
+		<h2 className="danger">Characters</h2>
+		{store.people.map((person) => (
+			<div className="card-container">
+				<div className="card w-25">
+					<img src="https://upload.wikimedia.org/wikipedia/commons/6/6c/Star_Wars_Logo.svg" className="card-img-top" alt="..."/>
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<div className="d-flex flex-column mb-3">
+							<span>Gender: {person.gender}</span>
+							<span>Hair Color: {person.hair_color}</span>
+							<span>Eye-color: {person.eye_color}</span>
+						</div>	
+						<a href="#" className="btn btn-primary">Learn more</a>
+					</div>
+				</div>
+			</div>
+		)
+		)}
+		
+		<h2>Planets</h2>
+		<h2>Vehicles</h2>
+
+
 	</div>
-);
+	)
+	
+
+};
